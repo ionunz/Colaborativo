09db83ab374edaa6ef1485c9f08be6925d6b2720
Rafaelolascoaga <56174171+Rafaelolascoaga@users.noreply.github.com>
2019-10-23 21:10:15 -0500

    primera parte
    Ya inicie el codigo del html, y el Js, en el css solo puse lo que recuerdo...

---
+++ proyecto compartido/index.js
@@ -0,0 +1,7 @@
+{
+    $()
+let imagenAdress(this).attr('src');
+$('.showroom img').attr('scr', nombre de la imagen);
+$('.showroom img').removeclass('scr', nombre de la imagen);
+$('.showroom img').addclass('scr', nombre de la imagen);
+}
